8b84ba6daf8e6255e5524528c6fdae277bef626f
nikolasfil <nikolas13in@gmail.com>
2023-05-02 19:30:00 +0300

    exerc 1

---
+++ gr.upatas.project.exerc1/src/main/java/gr/upatas/project/junit/exerc1/MyMain.java
@@ -0,0 +1,10 @@
+package gr.upatas.project.junit.exerc1;
+
+public class MyMain {
+    public static void main(String[] args) {
+        MyClass myClass = new MyClass();
+        String result = myClass.sub(5, 3);
+        System.out.println("Result is " + result);
+        
+    }  
+}
